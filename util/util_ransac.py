5d02ac7b2ff67f4563c34496b28e9b83f0a69365
victor-amblard <victor.amblard@mines-paristech.fr>
2021-03-30 21:03:58 +0200

    Added RANSAC benchmark

---
+++ util/util_ransac.py
@@ -0,0 +1,18 @@
+import open3d as o3d 
+
+# Code snippet taken from open3d documentation
+def ransac_registration(source_down, target_down, source_feats, target_feats, voxel_size, max_iter = 10000):
+
+    distance_threshold = voxel_size * 1.5
+    print(":: RANSAC registration on downsampled point clouds.")
+    print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    print("   we use a liberal distance threshold %.3f." % distance_threshold)
+    result = o3d.registration.registration_ransac_based_on_feature_matching(
+        source_down, target_down, source_feats, target_feats, distance_threshold,
+        o3d.registration.TransformationEstimationPointToPoint(False), 4, [
+            o3d.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+            o3d.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
+        ], o3d.registration.RANSACConvergenceCriteria(max_iter, 500))
+
+    return result.transformation
